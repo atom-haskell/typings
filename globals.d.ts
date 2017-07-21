4a5256e5f07974b0805ab647a297b0fdb3b3fbc1
Nikolay Yakimov <root@livid.pp.ru>
2017-07-22 00:18:46 +0300

    Remove obsolete typing

--- globals.d.ts
+++ globals.d.ts
@@ -2,10 +2,6 @@ interface Function {
   bind<T> (this: T, thisArg: any): T;
 }
 
-interface Array<T> {
-  map<U, V>(callbackfn: (value: T, index: number, array: T[]) => [U, V], thisArg?: any): [U, V][];
-}
-
 interface ObjectConstructor {
     keys<K extends string>(o: {[Key in K]: any}): K[];
     entries<K extends string, T>(o: { [Key in K]: T }): [K, T][];
