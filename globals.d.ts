d850637fde8ad09d3832c451f37abb44d3f09322
Nikolay Yakimov <root@livid.pp.ru>
2017-07-22 00:00:42 +0300

    Stricter typings for Object.keys and Object.entries

--- globals.d.ts
+++ globals.d.ts
@@ -5,3 +5,8 @@ interface Function {
 interface Array<T> {
   map<U, V>(callbackfn: (value: T, index: number, array: T[]) => [U, V], thisArg?: any): [U, V][];
 }
+
+interface ObjectConstructor {
+    keys<K extends string>(o: {[Key in K]: any}): K[];
+    entries<K extends string, T>(o: { [Key in K]: T }): [K, T][];
+}
